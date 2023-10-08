e6570a25238946d0e50222de4ee6243fef6f8b56
matej <matej.basta@email.cz>
2023-10-08 18:22:25 +0200

    secure communication

--- sourceCode/Client.java
+++ sourceCode/Client.java
@@ -7,6 +7,10 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.nio.Buffer;
 
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
 public class Client {
     public static void main(String[] args) {
         if (args.length != 3) {
@@ -14,16 +18,21 @@ public class Client {
             System.exit(1);
         }
 
+        System.setProperty("javax.net.ssl.trustStore", "myTrustedStore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "password");
+        // System.setProperty("javax.net.debug", "all");
         System.out.println("Client started");
         String message = args[0];
         String serverIP = args[1];
         int serverPort = Integer.parseInt(args[2]);
 
         try {
-            Socket socket = new Socket(serverIP, serverPort);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory
+                    .getDefault();
+            SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket(serverIP, serverPort);
+            PrintWriter out = new PrintWriter(sslSocket.getOutputStream(), true);
             out.println(message);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
             System.out.println(in.readLine());
         } catch (UnknownHostException e) {
             e.printStackTrace();
--- sourceCode/Server.java
+++ sourceCode/Server.java
@@ -4,15 +4,24 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.Security;
+
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
 
 public class Server {
     public static void main(String[] args) {
+        int port = 7007;
+        System.setProperty("javax.net.ssl.keyStore", "keystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "password");
+        // System.setProperty("javax.net.debug", "all");
         System.out.println("Waiting for clients...");
-
         try {
-            ServerSocket serverSocket = new ServerSocket(7007);
+            SSLServerSocketFactory sslServerSocketFactory = (SSLServerSocketFactory) SSLServerSocketFactory
+                    .getDefault();
+            SSLServerSocket sslServerSocket = (SSLServerSocket) sslServerSocketFactory.createServerSocket(port);
             while (true) {
-                Socket socket = serverSocket.accept();
+                Socket socket = sslServerSocket.accept();
                 System.out.println("Connection established");
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String message = in.readLine();
