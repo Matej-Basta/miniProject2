95d273619db8c043d2ac39b34782fe3aa89d3a53
matej <matej.basta@email.cz>
2023-10-08 20:01:24 +0200

    unsecure implementation to compare results

---
+++ sourceCode/UnsecureClient.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.Buffer;
+
+public class UnsecureClient {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: java Client <message> <server IP> <server port>");
+            System.exit(1);
+        }
+
+        System.out.println("Client started");
+        String message = args[0];
+        String serverIP = args[1];
+        int serverPort = Integer.parseInt(args[2]);
+
+        try {
+            Socket socket = new Socket(serverIP, serverPort);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(message);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println(in.readLine());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
